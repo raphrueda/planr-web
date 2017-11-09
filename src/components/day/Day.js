7c2135b7999113d20a577e237fbb2fc0d43c6e26
raph <raphrueda@gmail.com>
2017-11-09 17:44:20 +1100

    protyping event slots

--- src/components/day/Day.js
+++ src/components/day/Day.js
@@ -6,23 +6,46 @@ class Day extends Component {
     super(props);
     console.log("test");
 
+    // Function binds
     this.onSelected = this.onSelected.bind(this);
+    this.renderEvents = this.renderEvents.bind(this);
   }
 
+  // Event handlers
   onSelected() {
     this.props.handler(this.props.id);
   }
 
+  // Rendering
+
+  renderEvents() {
+    /*
+
+     */
+    return(
+      <div className="event-slots">
+        <div className="event-slot">
+
+        </div>
+        <div className="event-slot">
+
+        </div>
+      </div>
+    )
+  }
+
   render(){
     return(
-      <div
-        className={ ""
+      <div className={ ""
           + " col"
           + " calendar-day"
           + (this.props.selectedDay ? " selected-day" : "")
           + (this.props.currMonth ? "" : " other-month")}
         onClick={this.onSelected}>
-        <p>{this.props.date}</p>
+        {this.renderEvents()}
+        <div className="day-label">
+          <p>{this.props.date}</p>
+        </div>
       </div>
     )
   }
